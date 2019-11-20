314b2780c1ddc8ee5ad5776688467af5a175d1c1
MoonSulong <szhou78@wisc.edu>
2019-11-19 23:24:56 -0600

    update servlet & youtube controller

---
+++ YouTube/src/main/java/youtube/controller/YouTubeSearchController.java
@@ -0,0 +1,34 @@
+package youtube.controller;
+
+import java.util.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import youtube.external.YouTubeClient;
+import youtube.model.Video;
+
+@Controller
+public class YouTubeSearchController {
+	
+	@ResponseBody
+	@RequestMapping(value = "/search/{query}", method = RequestMethod.GET)   
+	// path variable with {username} in the url @PathVariable("username") String name
+	// @PathVariabl Map<String, String> pathVars key- "username" value - input in url
+	
+	public ArrayList<Video> getVideoList(@PathVariable("query") String term) {
+
+		
+		ArrayList<Video> videoList = YouTubeClient.getVideoList(term);
+		
+		
+		return videoList;
+		
+	}
+}
