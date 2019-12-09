8eef13dcf76abd619f9b3066ad762ebb37aba5e1
xi <sissilx5@gmail.com>
2019-12-08 16:28:19 -0800

    delete oldfiles`:wq
    enter the commit message for your changes. Lines starting

--- YouTube/src/main/java/youtube/dao/TagDao.java
+++
@@ -1,8 +0,0 @@
-package youtube.dao;
-
-import youtube.model.Tag;
-
-public interface TagDao {
-	void addTag(Tag tag);
-	//void removeTag(String tagName);
-}
--- YouTube/src/main/java/youtube/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package youtube.service;
-
-import youtube.model.User;
-
-public interface UserService {
-	void addUser(User user);
-	User getUser(String name);
-}
